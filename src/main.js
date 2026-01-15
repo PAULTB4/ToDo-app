960dfa22fa97e35ba4a0c586f837f17bee5ecc0c
PAULTB4 <paul.tarazona@unas.edu.pe>
2026-01-15 18:57:24 -0500

    listo el imnput

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './style.css';
 import { App } from './todos/app';
 import todoStore from './store/todo.store'; //Hacemo un expotacion por deault
-
 todoStore.initStore();
 App('#app');
--- src/todos/app.js
+++ src/todos/app.js
@@ -4,6 +4,8 @@ import { renderTodos } from './use-cases';
 
 const elementIDs = {
     TodoList: '.todo-list',
+    NewToDoInput: '#new-todo-input',
+    DeleteToDo: '.destroy'
 
 }
 
@@ -28,4 +30,33 @@ export const App = (elementId) => {
         displayTodos();
     })();
 
+    //referencias HTML
+    const newDescriptionInput = document.querySelector(elementIDs.NewToDoInput);
+    const todoListUL = document.querySelector(elementIDs.TodoList);
+
+
+    //listeners
+    newDescriptionInput.addEventListener('keyup', () => {
+        if (event.keyCode !== 13) return;
+        if (event.target.value.trim().length === 0) return;
+        todoStore.addToo(event.target.value);
+        displayTodos();
+        event.target.value = "";
+    })
+
+    todoListUL.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        console.log(event.target)
+        displayTodos();
+    })
+    todoListUL.addEventListener('click', (event) => {
+        const isDestroyElement = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+
+        if (!element || !isDestroyElement) return;
+        todoStore.deleteTodo(element.getAttribute('data-id'));
+
+        displayTodos();
+    })
 }
